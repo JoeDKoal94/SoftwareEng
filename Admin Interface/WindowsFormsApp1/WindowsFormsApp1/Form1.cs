f21051357e607595394d904b7badcfbb288dc96c
JoeDKoal94 <51380575+JoeDKoal94@users.noreply.github.com>
2019-06-18 13:49:58 -0500

    Admin Interface

---
+++ Admin Interface/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class adminInterface : Form
+    {
+        public adminInterface()
+        {
+            InitializeComponent();
+        }
+
+        private void addEditStudent_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            addEditInfoForm admin = new addEditInfoForm();
+            admin.Show();
+        }
+    }
+}
