5c99dae4e51ef33f05dc9d599dc6215e4cde7637
ReneACS <51380650+ReneACS@users.noreply.github.com>
2019-06-20 20:13:08 -0500

    Add files via upload

---
+++ Connection V.5/Form3.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace Connection
+{
+     partial class Form3
+     {
+          /// <summary>
+          /// Required designer variable.
+          /// </summary>
+          private System.ComponentModel.IContainer components = null;
+
+          /// <summary>
+          /// Clean up any resources being used.
+          /// </summary>
+          /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+          protected override void Dispose(bool disposing)
+          {
+               if (disposing && (components != null))
+               {
+                    components.Dispose();
+               }
+               base.Dispose(disposing);
+          }
+
+          #region Windows Form Designer generated code
+
+          /// <summary>
+          /// Required method for Designer support - do not modify
+          /// the contents of this method with the code editor.
+          /// </summary>
+          private void InitializeComponent()
+          {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(13, 97);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1041, 317);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(284, 455);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "GPA";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(643, 455);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 28);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Schedule";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(459, 455);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(93, 27);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Grades";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(105, 30);
+            this.label1.MaximumSize = new System.Drawing.Size(400, 400);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 19);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(572, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(614, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "label3";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+          }
+
+          #endregion
+
+          private System.Windows.Forms.DataGridView dataGridView1;
+          private System.Windows.Forms.Button button1;
+          private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ Connection V.5/Form3.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Connection
+{
+     public partial class Form3 : Form
+     {
+          public Form3()
+          {
+               InitializeComponent();
+               load_table();
+                string temp = Form1.variable1;
+                label1.Text = temp;
+                load_labels();
+        }
+        void load_labels()
+        {
+            string constring = "datasource=localhost;Database=software_project;Uid=root;Pwd=zero00;";
+            MySqlConnection conDatabase = new MySqlConnection(constring);
+            conDatabase.Open();
+            string temp = Form1.variable1;
+            MySqlCommand cmdDataBase = new MySqlCommand("select first_name, last_name from students_tbl where UserName = @UserName;", conDatabase);
+            cmdDataBase.Parameters.Add(new MySqlParameter("UserName", temp));
+            MySqlDataReader dr = cmdDataBase.ExecuteReader();
+            if (dr.Read())
+            {
+
+                label2.Text = (dr["first_name"].ToString());
+                label3.Text = (dr["last_name"].ToString());
+            }
+        }
+        void load_table()
+          {
+            string constring = "datasource=localhost;Database=software_project;Uid=root;Pwd=zero00;";
+            MySqlConnection conDatabase = new MySqlConnection(constring);
+            string temp = Form1.variable1;
+            MySqlCommand cmdDataBase = new MySqlCommand("SELECT UserName,course_number, course_name, start_time, end_time, days FROM grades_tbl a, courses_tbl b WHERE a.UserName = @UserName AND a.course_id = b.course_id; ; ", conDatabase);
+            cmdDataBase.Parameters.Add(new MySqlParameter("UserName", temp));
+            try
+            {
+
+                MySqlDataAdapter sda = new MySqlDataAdapter();
+                sda.SelectCommand = cmdDataBase;
+                DataTable dbdataset = new DataTable();
+                sda.Fill(dbdataset);
+                BindingSource bSource = new BindingSource();
+
+                bSource.DataSource = dbdataset;
+                dataGridView1.DataSource = bSource;
+                sda.Update(dbdataset);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+          private void button1_Click(object sender, EventArgs e)
+          {
+            string constring = "datasource=localhost;Database=software_project;Uid=root;Pwd=zero00;";
+            MySqlConnection conDatabase = new MySqlConnection(constring);
+            string temp = Form1.variable1;
+            MySqlCommand cmdDataBase = new MySqlCommand("SELECT avg(a.credits) AS GPA FROM grades_tbl a, courses_tbl b WHERE a.UserName = @UserName AND a.course_id = b.course_id;", conDatabase);
+            cmdDataBase.Parameters.Add(new MySqlParameter("UserName", temp));
+            try
+            {
+
+                MySqlDataAdapter sda = new MySqlDataAdapter();
+                sda.SelectCommand = cmdDataBase;
+                DataTable dbdataset = new DataTable();
+                sda.Fill(dbdataset);
+                BindingSource bSource = new BindingSource();
+
+                bSource.DataSource = dbdataset;
+                dataGridView1.DataSource = bSource;
+                sda.Update(dbdataset);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+          {
+               string constring = "datasource=localhost;Database=software_project;Uid=root;Pwd=zero00;";
+               MySqlConnection conDatabase = new MySqlConnection(constring);
+               string temp = Form1.variable1;
+               MySqlCommand cmdDataBase = new MySqlCommand("SELECT course_number, course_name, start_time, end_time, days FROM grades_tbl a, courses_tbl b WHERE a.UserName = @UserName AND a.course_id = b.course_id; ; ", conDatabase);
+               cmdDataBase.Parameters.Add(new MySqlParameter("UserName", temp));
+               try
+               {
+
+                    MySqlDataAdapter sda = new MySqlDataAdapter();
+                    sda.SelectCommand = cmdDataBase;
+                    DataTable dbdataset = new DataTable();
+                    sda.Fill(dbdataset);
+                    BindingSource bSource = new BindingSource();
+
+                    bSource.DataSource = dbdataset;
+                    dataGridView1.DataSource = bSource;
+                    sda.Update(dbdataset);
+               }
+               catch (Exception ex)
+               {
+                    MessageBox.Show(ex.Message);
+               }
+          }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string constring = "datasource=localhost;Database=software_project;Uid=root;Pwd=zero00;";
+            MySqlConnection conDatabase = new MySqlConnection(constring);
+            string temp = Form1.variable1;
+            MySqlCommand cmdDataBase = new MySqlCommand("SELECT course_number, course_name, exam_1, exam_2, final, final_grade FROM grades_tbl a, courses_tbl b WHERE a.UserName = @UserName AND a.course_id = b.course_id; ; ", conDatabase);
+            cmdDataBase.Parameters.Add(new MySqlParameter("UserName", temp));
+            try
+            {
+
+                MySqlDataAdapter sda = new MySqlDataAdapter();
+                sda.SelectCommand = cmdDataBase;
+                DataTable dbdataset = new DataTable();
+                sda.Fill(dbdataset);
+                BindingSource bSource = new BindingSource();
+
+                bSource.DataSource = dbdataset;
+                dataGridView1.DataSource = bSource;
+                sda.Update(dbdataset);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
